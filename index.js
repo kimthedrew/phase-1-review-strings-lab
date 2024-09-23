41d0552d1e5499279eabc4a7b2e5642d933fac08
kimthedrew <kimutai848@gmail.com>
2024-09-23 08:16:32 -0400

    strings

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+console.log(currentUser)
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+console.log(welcomeMessage)
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+console.log(excitedWelcomeMessage)
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+console.log(shortGreeting)
